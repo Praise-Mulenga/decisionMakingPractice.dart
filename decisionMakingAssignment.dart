451bc0b589ce626ae7c2274b6c51774f8ec38278
Praise Mulenga <praisemulengas@gmail.com>
2023-02-20 19:21:25 +0200

    first commit
    Basic decision making assignment - PLP

---
+++ decisionMakingAssignment.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main() {
+  int num = int.parse(stdin.readLineSync()!);
+  if (num > 10) {
+    print("Your number is greater than 10");
+  } else if (num < 10) {
+    print("Your number is less than 10");
+  } else {
+    print("Your number is equal to 10");
+  }
+}
